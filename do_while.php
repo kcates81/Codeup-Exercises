1862d27371fa928153d02edd92880dab5b69c153
kcates81 <kriscates81@gmail.com>
2016-04-15 13:31:45 -0500

    added 2 more do-while loops that count backwards from 100 and end at -10 and that multiply the variable by itself until the variable is less than 1,000,000

--- do_while.php
+++ do_while.php
@@ -7,3 +7,21 @@ do {
     $a += 2;
     echo $a.PHP_EOL;
 } while ($a <= 98);
+
+// this do while loop counts backwards by 5s from 100 to -10
+
+$b = 105;
+
+do {
+    $b-=5;
+    echo $b.PHP_EOL;
+}while ($b >= -5);
+
+// this do while loop multiplies the variable $a by itself until it reaches 1,000,000
+
+$c = 2;
+
+do {
+    echo $c.PHP_EOL;
+    $c *= $c;
+} while ($c < 1000000);
