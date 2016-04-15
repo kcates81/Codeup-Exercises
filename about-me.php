5b6d5f003be0672d84d9cbc991d5bea2d4bd8f5b
kcates81 <kriscates81@gmail.com>
2016-04-15 11:29:21 -0500

    examples of else/if statements

--- about-me.php
+++ about-me.php
@@ -1,14 +1,12 @@
 <?php
 
-$a = 5;
-$b = 10;
+$a = 10;
+$b = '10';
 
-// standard syntax
-if ($a == $b) {
+if ($a === $b) {
+    echo "$a is identical to $b";
+} elseif ($a == $b) {
     echo "$a is equal to $b";
 } else {
     echo "$a is not equal to $b";
-}
-
-// ternary operator 
-echo $a == $b ? "$a is equal to $b" : "$a is not equal to $b\n";
+}  
