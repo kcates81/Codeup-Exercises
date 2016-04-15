bdf7de5cd36b02585700f1ec2eb03ef91ab55e4c
kcates81 <kriscates81@gmail.com>
2016-04-15 11:17:35 -0500

    examples of ternary operators and if/else statements

--- about-me.php
+++ about-me.php
@@ -1,5 +1,14 @@
 <?php
 
-define('MY_NAME', 'Kristen');
+$a = 5;
+$b = 10;
 
-echo 'My name is '.MY_NAME.PHP_EOL;
+// standard syntax
+if ($a == $b) {
+    echo "$a is equal to $b";
+} else {
+    echo "$a is not equal to $b";
+}
+
+// ternary operator 
+echo $a == $b ? "$a is equal to $b" : "$a is not equal to $b\n";
