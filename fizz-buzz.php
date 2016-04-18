34abd116ca0501640b3bd915617f4a5c83a85544
kcates81 <kriscates81@gmail.com>
2016-04-18 14:15:32 -0500

    got the game to work using a for loop

--- fizz-buzz.php
+++ fizz-buzz.php
@@ -1,5 +1,13 @@
 <?php 
 
-for ($i=0; $i <= 100 ; $i++) { 
-    echo "The number = {$i}\n";
+for ($i = 1; $i <= 100 ; $i++) { 
+    if ($i % 3 == 0 && $i % 5 == 0) {
+        echo "Fizzbuzz\n";
+    } elseif ($i % 5 == 0) {
+        echo "Buzz\n";
+    } elseif ($i % 3 == 0) {
+        echo "Fizz\n";
+    } else {
+        echo "{$i}\n";
+    }
 }
