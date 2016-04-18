15ecb2217335beb0929953d9c620b0aff6f87c7b
kcates81 <kriscates81@gmail.com>
2016-04-18 11:39:14 -0500

    added examples of a for loop

--- about-me.php
+++ about-me.php
@@ -1,8 +1,11 @@
 <?php
 
-// Passing Command Line Arguments 
-if ($argc == 3) {
-    echo "arg1 is {$argv[1]} and arg2 is {$argv[2]}";
-};
+// for loop example
+for ($a = 1; $a <= 5; $a++) {
+    echo "\$a has a value of {$a}\n";
+}
 
-var_dump($argv);
+// for loop that increments by 2
+for ($i = 0; $i <= 100; $i += 2) {
+    echo "\$i has a value of {$i}\n";
+}
