a92aff4b06fe39341730f0af49bdb266f7f48100
kcates81 <kriscates81@gmail.com>
2016-04-18 14:21:55 -0500

    added comments

--- fizz-buzz.php
+++ fizz-buzz.php
@@ -1,12 +1,16 @@
 <?php 
 
 for ($i = 1; $i <= 100 ; $i++) { 
+    // checks if the number is a multiple of both 3 AND 5
     if ($i % 3 == 0 && $i % 5 == 0) {
         echo "Fizzbuzz\n";
+    // checks if the number is a multiple of 5
     } elseif ($i % 5 == 0) {
         echo "Buzz\n";
+    // checks if the number if a multiple of 3
     } elseif ($i % 3 == 0) {
         echo "Fizz\n";
+    // default case
     } else {
         echo "{$i}\n";
     }
