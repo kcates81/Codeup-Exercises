1ba276dbd2f3ff28ffc0fa4d540882a1f91609f6
kcates81 <kriscates81@gmail.com>
2016-04-18 14:37:56 -0500

    added example of foreach

--- about-me.php
+++ about-me.php
@@ -1,11 +1,15 @@
 <?php
 
-// for loop example
-for ($a = 1; $a <= 5; $a++) {
-    echo "\$a has a value of {$a}\n";
-}
+// example of a foreach
+$names = [
+    "Lorena",
+    "Melissa",
+    "Nikki",
+    "Edric",
+    "Marcus",
+    "Pancho"
+];
 
-// for loop that increments by 2
-for ($i = 0; $i <= 100; $i += 2) {
-    echo "\$i has a value of {$i}\n";
+foreach ($names as $studentName) {
+   echo "Hey everybody, say hello to {$studentName}\n";
 }
