cdb0ba6f83b9e14094638eb8f5cb64d4f03b68b7
kcates81 <kriscates81@gmail.com>
2016-04-18 13:58:04 -0500

    redid my code. Got it to work.

--- for.php
+++ for.php
@@ -1,14 +1,19 @@
 <?php
 
-// Prompting the user to enter a starting and ending number
-echo "Please enter a starting and an ending number\n";
+// This is the initial prompt. The user has to run the file in the 
+// terminal w/o arguments to get this to work
+if ($argc == 1) {
+    echo "Please enter a starting and ending number\n";
+    exit;
+}
 
-if ($argc == 3 && ctype_digit($argv[1]) && ctype_digit($argv[2])) {
-        $startingNum = $argv[1];
-        $endingNum = $argv[2];
+// This prompts the user to input an increment for the number output
+fwrite(STDOUT, "What is the increment?\n");
+$increment = fgets(STDIN);
 
-       $range = (range ((int)$startingNum, (int)$endingNum));
-
-       var_dump($range);
-    }
+// This sets the min and max for the number range. The user has to 
+// load the file and pass 2 numbers as arguments for this to work
+for ($min = $argv[1]; $min <= $argv[2] ; $min += $increment) { 
+   echo "number = {$min}\n";
+}
 
