a013380e4931c508a73a2ce3062eff69ad08a0df
kcates81 <kriscates81@gmail.com>
2016-04-18 15:14:25 -0500

    changed foreach lop to output all of the contents of the array included the items within the nested array

--- foreach.php
+++ foreach.php
@@ -10,24 +10,14 @@ $things = array('Sgt. Pepper',
     (string) 11)
 ;
 
-// foreach loop that checks if an item is a scalar 
+// foreach loop that echoes all of the items within the array including the contents of the nested array
+
 foreach ($things as $item) {
-   if (is_integer($item)) {
-       echo "{$item} is an integer\n";
-    } elseif (is_float($item)) {
-       echo "{$item} is a float\n";
-    } elseif (is_bool($item)) {
-       echo "{$item} is a boolean\n";
-    } elseif (is_array($item)) {
-        echo "This is an array: \n";
+    if (is_array($item)) {
        foreach ($item as $element) {
-              echo "$element\n";
-          }   
-    } elseif (is_null($item)) {
-       echo "{$item} is null\n";
-    } elseif (is_string($item)) {
-        echo "{$item} is a string\n";
-    } elseif (is_scalar($item)) {
-        echo "{$item}is a scalar\n";
-    }
+            echo "$element\n";
+        } 
+    } else {
+        echo "$item\n";
+    } 
 }
