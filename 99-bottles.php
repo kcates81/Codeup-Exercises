2beaeedf47cb43ab90f8ac11ab420c24dfecdf4b
kcates81 <kriscates81@gmail.com>
2016-04-18 21:14:11 -0500

    added timer so the output increments every 1 second. also restarted i at 100 when the lyrics reach zero so that the output restarts at 99

--- 99-bottles.php
+++ 99-bottles.php
@@ -4,8 +4,19 @@ for ($i= 99; $i >= 0 ; $i--) {
     echo "{$i} bottles of beer on the wall, {$i} bottles of beer\n";
     echo "Take one down, pass it around\n";
 
+    usleep(1000000);
+
     if ($i == 1) {
         echo "{$i} bottle of beer on the wall, {$i} bottle of beer\n";
         echo "Take it down, pass it around\n";
     }
+
+    if ($i == 0) {
+        echo "{$i} bottles of beer on the wall, {$i} bottles of beer\n";
+
+        echo "Go to the store, buy some more\n";
+
+        $i = 100;
+    }
+
 }
