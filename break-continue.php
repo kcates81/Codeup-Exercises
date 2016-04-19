f31e9dd8bf746ca4ec8630c7d404a61d49ce8be4
kcates81 <kriscates81@gmail.com>
2016-04-19 11:11:45 -0500

    code was printing even numbers with or without continue. moved echo under the if statement and changed conditional so that the modulus would not be equal to 0. this fixed the issue

--- break-continue.php
+++ break-continue.php
@@ -2,11 +2,11 @@
 
 for ($i=0; $i <= 100; $i++) { 
     
-    if ($i %2 == 0) {
-        echo "$i\n";
+    if ($i % 2 != 0) {
         continue;
     }
-    
+
+    echo "$i\n"; 
 }
 
 for ($i=0; $i <= 100 ; $i++) { 
