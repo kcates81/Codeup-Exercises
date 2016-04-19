e7d87eeba875ec34bdd93d91cf10a82513934266
kcates81 <kriscates81@gmail.com>
2016-04-18 21:00:54 -0500

    added song lyrics. also added if statement to facilitate grammar change when only one bottle of beer left

--- 99-bottles.php
+++ 99-bottles.php
@@ -1,5 +1,11 @@
 <?php
 
 for ($i= 99; $i >= 0 ; $i--) { 
-    echo "$i\n";
+    echo "{$i} bottles of beer on the wall, {$i} bottles of beer\n";
+    echo "Take one down, pass it around\n";
+
+    if ($i == 1) {
+        echo "{$i} bottle of beer on the wall, {$i} bottle of beer\n";
+        echo "Take it down, pass it around\n";
+    }
 }
