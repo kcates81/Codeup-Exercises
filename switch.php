9ea8d18bc6b09ca8db46957d0c92e3da28681fa9
kcates81 <kriscates81@gmail.com>
2016-04-19 11:35:29 -0500

    initial commit.

---
+++ switch.php
@@ -0,0 +1,34 @@
+<?php
+
+// Set the default timezone
+date_default_timezone_set('America/Chicago');
+
+// Get Day of Week as number
+// 1 (for Monday) through 7 (for Sunday)
+$dayOfWeek = date('N');
+
+var_dump($dayOfWeek).PHP_EOL;
+
+switch($dayOfWeek) {
+    case 1:
+        echo "Monday\n";
+        break;
+    case 2:
+        echo "Tuesday\n";
+        break;
+    case 3:
+        echo "Wednesday\n";
+        break;
+    case 4:
+        echo "Thursday\n";
+        break;
+    case 5:
+        echo "Friday\n";
+        break;
+    case 6:
+        echo "Saturday\n";
+        break;
+    case 7:
+        echo "Sunday\n";
+        break;
+ };
