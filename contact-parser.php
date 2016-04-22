2441f7cc7e8d9b05c4b722c361a3fba797ecf86a
kcates81 <kriscates81@gmail.com>
2016-04-22 11:06:42 -0500

    added comments

--- contact-parser.php
+++ contact-parser.php
@@ -2,9 +2,10 @@
 
 function parseContacts($filename)
 {
+    // this is my empty array of contacts
     $contacts = array();
 
-    // todo - read file and parse contacts
+    //this code pulls my content out of my text file and assigns it to the $contacts array
     $filename = 'contacts.txt';
 
     $handle = fopen('contacts.txt', 'r');
@@ -13,29 +14,40 @@ function parseContacts($filename)
 
     fclose($handle);
 
+    // this code formats my array
     $contacts = explode("\n", trim($contents));
 
+    // empty array I'll use later to push content from the $contacts array into once I've manipulated the data 
     $newArray = array();
+
     foreach ($contacts as $key => $value) {
 
-        $details = explode('|', $value); // correct
+        // this separates the name from the number as two different keys within my array
+        $details = explode('|', $value);
 
+        // this pulls out the area code from the string of numbers
         $areaCode = substr($details[1], -10, 3);
 
+        // this pulls out the exchange number from the string of numbers
         $exchange = substr($details[1], -7, 3);
 
+        // this pulls the SLN out of the string of numbers
         $sln = substr($details[1], -4, 4);
 
+        // this concatenates the area code, exchange, and SLN, back together as one phone number with dashes between them
         $number = $areaCode.'-'.$exchange.'-'.$sln;
 
+        // this populates my contacts array with name and number as my keys and assigns the proper values to each key
         $contacts = array('name' => $details[0], 'phone' => $number);
 
-        array_push($newArray, $contacts);
-        
+        // this pushes my newly formatted $contacts array into my $newArray
+        array_push($newArray, $contacts);  
     }
 
+    // this returns $newArray
     return $newArray;
 
 }
 
+// this runs my function in the REPL. I can now use this function on any file
 var_dump(parseContacts('contacts.txt'));
